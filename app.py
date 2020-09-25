781078c5b3651501bbe85075d0db9fde1a0260bb
junnii <43146690+junnii@users.noreply.github.com>
2020-09-26 05:19:18 +0900

    Add files via upload
    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from flask import Flask, render_template, request, logging, Response, redirect, flash
 from config import CONFIG
 
-# 各種ツイッターのキーをセット
+#
 CONSUMER_KEY = CONFIG["CONSUMER_KEY"]
 CONSUMER_SECRET = CONFIG["CONSUMER_SECRET"]
 ACCESS_TOKEN = CONFIG["ACCESS_TOKEN"]
@@ -11,13 +11,13 @@ ACCESS_SECRET = CONFIG["ACCESS_SECRET"]
  #Tweepy
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
- #APIインスタンスを作成
+ #
 api = tweepy.API(auth)
 
-# Flask の起動
+#
 app = Flask(__name__)
 
-# Viewの処理
+#
 columns = [
    "tweet_id",
    "created_at",
@@ -44,7 +44,11 @@ def index():
        return render_template('index.html')
 def get_tweets_df(user_id):
    tweets_df = pd.DataFrame(columns=columns) #1
+   i=0
    for tweet in tweepy.Cursor(api.user_timeline,screen_name = user_id, exclude_replies = True).items(): #2
+       i+=1
+       if i==60:
+           break 
        try:
            if not "RT @" in tweet.text: #3
                se = pd.Series([ #4
@@ -57,18 +61,20 @@ def get_tweets_df(user_id):
                    ,columns
                    )
                tweets_df = tweets_df.append(se,ignore_index=True) #5
+            
        except Exception as e:
            print (e)
+      
    tweets_df["created_at"] = pd.to_datetime(tweets_df["created_at"]) #6
    return tweets_df #7
-
+    
 def get_profile(user_id):
    user = api.get_user(screen_name= user_id) #1
    profile = { #2
        "id": user.id,
        "user_id": user_id,
        "image": user.profile_image_url,
-       "description": user.description # 自己紹介文の取得
+       "description": user.description #
    }
    return profile #3
 
